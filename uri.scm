9f56ad7a87b496b2544e723b44d67ec5d2cd2b09
Taylor R Campbell <campbell@mumble.net>
2007-08-06 17:50:53 +0000

    Omit URI merging tests from uri.scm; they are now in test-uri.scm.

--- uri.scm
+++ uri.scm
@@ -335,81 +335,6 @@
                       (else
                        (cons segment output-path))))))))
 
-;;;;; Testing URI Merging
-
-(define (test-uri-merging)
-  (let ((base-uri (string->uri "http://a/b/c/d;p?q")))
-    (define (test examples)
-      (filter-map (lambda (example)
-                    (let ((relative (car example))
-                          (expected (cadr example)))
-                      (let ((merged
-                             (uri->string (merge-uris relative base-uri #t))))
-                        (if (not (string=? merged expected))
-                            `(,relative ,merged ,expected)
-                            #f))))
-                  examples))
-    (let ((failures:normal (test test-uri-merging-examples:normal))
-          (failures:abnormal (test test-uri-merging-examples:abnormal)))
-      (if (and (null? failures:normal)
-               (null? failures:abnormal))
-          'PASS
-          `(FAIL (NORMAL-EXAMPLES ,@failures:normal)
-                 (ABNORMAL-EXAMPLES ,@failures:abnormal))))))
-
-;;; Taken from the RFC, pages 35-37.
-
-(define test-uri-merging-examples:normal
-  ;; Format is (RELATIVE EXPECTED-MERGED).
-  '(
-    ("g:h"      "g:h")
-    ("g"        "http://a/b/c/g")
-    ("./g"      "http://a/b/c/g")
-    ("g/"       "http://a/b/c/g/")
-    ("/g"       "http://a/g")
-    ("//g"      "http://g")
-    ("?y"       "http://a/b/c/d;p?y")
-    ("g?y"      "http://a/b/c/g?y")
-    ("#s"       "http://a/b/c/d;p?q#s")
-    ("g#s"      "http://a/b/c/g#s")
-    ("g?y#s"    "http://a/b/c/g?y#s")
-    (";x"       "http://a/b/c/;x")
-    ("g;x"      "http://a/b/c/g;x")
-    ("g;x?y#s"  "http://a/b/c/g;x?y#s")
-    (""         "http://a/b/c/d;p?q")
-    ("."        "http://a/b/c/")
-    ("./"       "http://a/b/c/")
-    (".."       "http://a/b/")
-    ("../"      "http://a/b/")
-    ("../g"     "http://a/b/g")
-    ("../.."    "http://a/")
-    ("../../"   "http://a/")
-    ("../../g"  "http://a/g")
-    ))
-
-(define test-uri-merging-examples:abnormal
-  '(
-    ("../../../g"       "http://a/g")
-    ("../../../../g"    "http://a/g")
-    ("/./g"             "http://a/g")
-    ("/../g"            "http://a/g")
-    ("g."               "http://a/b/c/g.")
-    (".g"               "http://a/b/c/.g")
-    ("g.."              "http://a/b/c/g..")
-    ("..g"              "http://a/b/c/..g")
-    ("./../g"           "http://a/b/g")
-    ("./g/."            "http://a/b/c/g/")
-    ("g/./h"            "http://a/b/c/g/h")
-    ("g/../h"           "http://a/b/c/h")
-    ("g;x=1/./y"        "http://a/b/c/g;x=1/y")
-    ("g;x=1/../y"       "http://a/b/c/y")
-    ("g?y/./x"          "http://a/b/c/g?y/./x")
-    ("g?y/../x"         "http://a/b/c/g?y/../x")
-    ("g#s/./x"          "http://a/b/c/g#s/./x")
-    ("g#s/../x"         "http://a/b/c/g#s/../x")
-    ("http:g"           "http:g")
-    ))
-
 ;;;; String<->URI Conversion
 
 (define (uri->string uri)
